791ebfd1ce5ce39705c85d794d73191850f22d66
Ana Luiza Félix <luizafelix97@outlook.com>
2021-03-06 03:18:35 -0300

    Initial commit

---
+++ src/main/java/br/com/sistemadecadastro/dao/DepartamentoDao.java
@@ -0,0 +1,20 @@
+package br.com.sistemadecadastro.dao;
+
+import br.com.sistemadecadastro.domain.Departamento;
+
+import java.util.List;
+
+
+
+public interface DepartamentoDao {
+
+    void save(Departamento departamento);
+
+    void update(Departamento departamento);
+
+    void delete(Long id);
+
+    Departamento findById(Long id);
+
+    List<Departamento> findAll();
+}
---
+++ src/main/java/br/com/sistemadecadastro/service/CargoService.java
@@ -0,0 +1,22 @@
+package br.com.sistemadecadastro.service;
+
+import br.com.sistemadecadastro.domain.Cargo;
+
+import java.util.List;
+
+
+
+public interface CargoService {
+
+	void salvar(Cargo cargo);
+	
+	void editar(Cargo cargo);
+	
+	void excluir(Long id);
+	
+	Cargo buscarPorId(Long id);
+	
+	List<Cargo> buscarTodos();
+	
+	boolean cargoTemFuncionarios(Long id);
+}
